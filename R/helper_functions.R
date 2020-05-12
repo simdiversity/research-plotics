d37f388453533cecc9de7f4a963ad51c990b4f70
Mattia Egloff <mattia.egloff@unil.ch>
2020-05-12 07:03:51 +0200

    Restructure

---
+++ R/helper_functions.R
@@ -0,0 +1,39 @@
+#' @importFrom utils data
+#' @importFrom here here
+NULL
+
+#'   Data name string to dataset
+#'
+#' Useful to load a dataset into a variable.
+#'
+#' @param dataset_name the name of the dataset to load
+#' @return the dataset
+#'
+#' @export
+dataset_from_str <- function(dataset_name) {
+  d <- new.env()
+  data(list = c(dataset_name), envir = d)
+  dataset <- get(dataset_name, envir = d)
+  rm(d)
+  dataset
+}
+
+#' Data name string to dataset
+#'
+#' Useful to load a dataset into a variable.
+#'
+#' @param new_name name of the data
+#' @param variable the original variable
+#' @param compress compression type see save
+#'
+#' @export
+save_with_name <- function(variable, new_name, compress = "gzip") {
+  assign(new_name, variable)
+  save(
+    list = c(new_name),
+    file = here::here("data", paste0(new_name, ".rda")),
+    compress = compress
+  )
+  invisible(variable)
+}
+
---
+++ scripts/01_compute_distances.R
@@ -0,0 +1,112 @@
+library(here)
+library(magrittr)
+library(dplyr)
+library(simdiversity.RNcEDGPS)
+library(simdiversity.data.politics)
+
+data_sets <- c(
+  data(package = "simdiversity.data.politics")
+)[["results"]][, "Item"]
+
+for (dataset_name in data_sets) {
+  data_set <- dataset_from_str(dataset_name)
+
+  for (option in seq(3)) {
+
+    if (option == 1) {
+      poll_codes <-
+        c(
+          `0` = 0, `1` = 1, `2` = NA_real_,
+          `3` = NA_real_, `4` = NA_real_,
+          `5` = NA_real_, `6` = NA_real_,
+          `7` = NA_real_
+        )
+    }
+
+    if (option == 2) {
+      poll_codes <-
+        c(
+          `0` = 0, `1` = 1, `2` = 0.5,
+          `3` = NA_real_, `4` = NA_real_,
+          `5` = NA_real_, `6` = NA_real_,
+          `7` = NA_real_
+        )
+    }
+    if (option == 3) {
+      poll_codes <-
+        c(
+          `0` = 0, `1` = 1, `2` = 0.5,
+          `3` = 0.5, `4` = NA_real_,
+          `5` = NA_real_, `6` = NA_real_,
+          `7` = NA_real_
+        )
+    }
+
+    scores_matrix <-
+      data_set$polls %>%
+      recode(!!!poll_codes) %>%
+      matrix(nrow = nrow(data_set$polls), dimnames = dimnames(data_set$polls))
+
+
+    file_name = paste0(dataset_name, "-", option, "-scores_matrix")
+    assign(file_name, scores_matrix )
+    save(
+      list = c(file_name),
+      file = here("data", paste0(file_name, ".rda")),
+      compress = "xz"
+    )
+
+    null_votes_index <- apply(scores_matrix, 2, function(x) {
+      all(is.na(x))
+    })
+    null_councillors_index <- apply(scores_matrix, 1, function(x) {
+      all(is.na(x))
+    })
+
+    null_votes <- c(names(which(null_votes_index)))
+    save_with_name(
+      null_votes,
+      paste0(dataset_name, "-", option, "-null_votes"),
+      compress = "gzip"
+    )
+
+
+    null_councilors <- c(names(which(null_councillors_index)))
+    save_with_name(
+      null_councilors,
+      paste0(dataset_name, "-", option, "-null_councilors"),
+      compress = "gzip"
+    )
+
+    scores_matrix <- scores_matrix[!null_councillors_index, !null_votes_index]
+    n <- nrow(scores_matrix)
+    p <- ncol(scores_matrix)
+
+    weight <- validity_weight(scores_matrix)
+    save_with_name(
+      weight,
+      paste0(dataset_name, "-", option, "-weight"),
+      compress = "gzip"
+    )
+
+    weighted_vote_disputedness <- disputedness(
+      scores_matrix, weight
+    )
+    save_with_name( weighted_vote_disputedness,
+      paste0(dataset_name, "-", option, "-weighted_vote_disputedness"),
+      compress = "gzip"
+    )
+
+    D_final <- estimate_distance(
+      scores_matrix, weight, weighted_vote_disputedness
+    )
+    save_with_name(D_final,
+      paste0(dataset_name, "-", option, "-D_final"),
+      compress = "xz"
+    )
+
+  gc()
+  }
+}
+
+
