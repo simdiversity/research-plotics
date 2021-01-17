a7080e6ef5241c0fd510d704ef347559c19df815
Mattia Egloff <mattia.egloff@unil.ch>
2021-01-17 13:13:14 +0100

    Removed Jenkins, added Github Actions instead

--- R/groups.R
+++ R/groups.R
@@ -2,22 +2,23 @@
 #'  importFrom stringr str_sort
 NULL
 
-#' @title Group dissimilarity
+#'
+#' @title group_dissimilarity
 #'
 #' @param dissimilarity A dissimilarity between individuals
 #' @param weight the weights of the individuals
 #' @param groups a group membership vector
 #'
 #' @export
-group_similarity <- function(dissimilarity, weight, groups) {
+group_dissimilarity <- function(dissimilarity, f, groups) {
   m <- length(unique(groups))
   parties = seq(m)
   names(parties) = stringr::str_sort(unique(groups))
   group_membership_matrix = as.matrix(dplyr::bind_cols(
     lapply(parties, function(i) as.numeric(parties[groups] == i))
   ))
-  group_weights = as.vector(t(weight) %*% group_membership_matrix) # weight of parties
-  emission_matrix = diag(weight) %*% group_membership_matrix %*% diag(1/group_weights)
+  group_weights = as.vector(t(f) %*% group_membership_matrix) # weight of parties
+  emission_matrix = diag(f) %*% group_membership_matrix %*% diag(1/group_weights)
   group_dissimilarity = matrix(NA, m, m)
   for (g in 1:m) {
     for (h in 1:m) {
